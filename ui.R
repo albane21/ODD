7096b313e8409481ca4c3a7a256cb6c152a4fe1d
albane21 <75802299+albane21@users.noreply.github.com>
2020-12-14 14:39:48 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,13 +1,12 @@
 
-# Chargement des librairies
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
 library(dplyr)
+library(markdown)
 library(tidyverse)
 library(hrbrthemes)
 library(viridis)
-loadfonts(dev="win")
 windowsFonts()
 
 # Lecture des données--------------------
@@ -21,7 +20,7 @@ trying = X18_25_non_insere[X18_25_non_insere$LIBGEO =="IDF"| X18_25_non_insere$L
 
     header <- 
         dashboardHeader(
-            title = HTML("Observatoire des Objectifs de Développement Durable en Ile-de-France"),
+            title = HTML("Observatoire des ODD IDF"),
             disable = FALSE,   	
             titleWidth = 300               
     )
@@ -37,18 +36,18 @@ header$children[[2]]$children[[1]] <- tags$a(href='http://www.driee.ile-de-franc
     dashboardSidebar(
             width = 200,
       sidebarMenu(
-        menuItem("Présentation de l'observatoire", tabName = "Présentation de l'observatoire"),
+        menuItem("Présentation de l'observatoire", tabName = "vue"),
         menuItem("Communes", tabName = "commune_choice", icon = icon("list-ol")),
-      "  div( id = 'sidebar_cr',
-      #       conditionalPanel("input.sidebar === 'commune_choice'",
-      #                         selectizeInput("Liste_Communes",
-      #                                        "Choisi la commune de ton choix", 
-      #                                        choices =  "LIBGEO", 
-      #                                        selected = NULL,  width = "200px",
-      #                                        multiple = T), #,
-      #                         actionButton('btn_build_country_report', 
-      #                                      paste0('Découvre ton bilan'),
-      #                                      icon = icon('wrench')))),
+        div( id = 'sidebar_cr',
+             conditionalPanel("input.sidebar === 'commune_choice'",
+                               selectizeInput("Liste_Communes",
+                                              "Choisi la commune de ton choix", 
+                                              choices =  "LIBGEO", 
+                                             selected = NULL,  width = "200px",
+                                              multiple = T), #,
+                              actionButton('btn_build_country_report', 
+                                            paste0('Découvre ton bilan'),
+                                            icon = icon('wrench')))),
         menuItem("ODD1", tabName = "ODD1"),
         menuItem("ODD2", tabName = "ODD2"),
         menuItem("ODD3", tabName = "ODD3"),
@@ -67,91 +66,61 @@ header$children[[2]]$children[[1]] <- tags$a(href='http://www.driee.ile-de-franc
       )
     )
 
- body <-   
-  dashboardBody(
-    tags$head(
-      # ## JS codes
-      # tags$script(src = "fixedElement.js" ),
-      # tags$style(HTML(".scroller_anchor{height:0px; margin:0; padding:0;}; 
-      #                  .scroller{background: white; 
-      #                   border: 1px solid #CCC; 
-      #                   margin:0 0 10px; 
-      #                   z-index:100; 
-      #                   height:50px; 
-      #                   font-size:18px; 
-      #                   font-weight:bold; 
-      #                   text-align:center; 
-      #                  width:500px;}")),
-      
-      #tags$script(src = "world.js" ),
-      tags$script("document.title = 'COMMUNE'"),
-
-
-      tabItems(
-        
-        
-        
-     
-        
-        #tabItem(tabName = "Présentation de l'observatoire",
-          
-          # home section
-          #includeMarkdown("www/home.md")
-          
-        #),
-          
-          
-        tabItem(
-          "vue",
-          box(
-            title = "Vaujours",
-            footer = textOutput("présentation_commune"),
-            status = "info",
-            solidHeader = TRUE,
-            width = 18
-          ),
-          box(
-            title = "ODD8: Taux des 18-25 ans non insérés",
-            footer = "Explication de l'indicateur",
-            status = "info",
-            solidHeader = TRUE,
-            width = 18,
-            plotOutput("barplot")
-          ),
-          box(
-            title = "ODD8: Taux des 18-25 ans non insérés",
-            footer = "Explication de l'indicateur",
-            status = "info",
-            solidHeader = TRUE,
-            width = 18,
-            plotOutput("evolution")
-          ),
-          infoBox(
-            title = "Evolution ODD1",
-            value = "en %",
-            subtitle = "Entre 2006 et 2016",
-            icon = icon("line-chart"),
-            fill = TRUE,
-            color = "light-blue",
-            width = 4
-          ),
-          valueBox(
-            value = "+ 38%",
-            subtitle = "Evolution sur 5 ans",
-            color = "green",
-            width = 4
-          ),
-          tabBox(
-            title = "Informations",
-            width = 4,
-            tabPanel(title = "Prix médian", "contenu 1"),
-            tabPanel(title = "Nombre", "contenu 2")
-          )
-        )
-     ))
+ body <-       
+ dashboardBody(
+             tabItems(
+             tabItem(
+             "vue",
+             box(
+             title = "Vaujours",
+             footer = textOutput("présentation_commune"),
+             status = "info",
+             solidHeader = TRUE,
+             width = 18
+             ),
+             box(
+             title = "ODD8: Taux des 18-25 ans non insérés",
+             footer = "Explication de l'indicateur",
+             status = "info",
+             solidHeader = TRUE,
+             width = 18,
+             plotOutput("barplot")
+             ),
+             box(
+             title = "ODD8: Taux des 18-25 ans non insérés",
+             footer = "Explication de l'indicateur",
+             status = "info",
+             solidHeader = TRUE,
+             width = 18,
+             plotOutput("evolution")
+             ),
+             infoBox(
+             title = "Evolution ODD1",
+             value = "en %",
+             subtitle = "Entre 2006 et 2016",
+             icon = icon("line-chart"),
+             fill = TRUE,
+             color = "light-blue",
+             width = 4
+             ),
+             valueBox(
+             value = "+ 38%",
+             subtitle = "Evolution sur 5 ans",
+             color = "green",
+             width = 4
+             ),
+             tabBox(
+             title = "Informations",
+             width = 4,
+             tabPanel(title = "Prix médian", "contenu 1"),
+             tabPanel(title = "Nombre", "contenu 2")
+             )
+             )
+             ),
+             skin = "red"
+             )
  
 
-## put UI together --------------------
 ui <- 
    dashboardPage(header , siderbar , body )
 
